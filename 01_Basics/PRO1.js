af06b826629e453f563d4032598c1837733f7a24
bodarkrish011-sys <bodarkrish011@gmail.com>
2026-05-31 04:14:29 +0000

    works with variable

--- 01_Basics/PRO1.js
+++ 01_Basics/PRO1.js
@@ -1,1 +1,18 @@
 console.log("Hello node")
+
+let num = 33
+let stringNum = String(num)
+console.log(typeof num) //  typeof used to find type of value 
+console.log(num)
+
+let str = "35313"
+let strToNum = Number(str)
+console.log(typeof strToNum)  // if str = "hello" then terminal shows Nan - Not a Number 
+console.log(strToNum)
+
+let Num = 1
+let NumToBole = Boolean(Num)
+console.log(typeof NumToBole)
+console.log(NumToBole)
+
+console.log(typeof 11.11)
